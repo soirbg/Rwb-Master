ee5c284681a9fbe134d11e845b7e577d888816f3
soir <stoyan.g.dobrev@gmail.com>
2017-02-02 11:32:38 +0200

    Third commit

--- server/config/express.js
+++ server/config/express.js
@@ -20,7 +20,7 @@ module.exports = (config, app) => {
   app.use(passport.session())
   app.use(function (req, res, next) {
 
-    
+    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
